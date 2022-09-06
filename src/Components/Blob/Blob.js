c6bd46dc8992fe8f9f0877b860c629b43b684c93
Dikshant Deoli <dikshantdevli@gmail.com>
2022-09-06 07:50:13 +0530

    blob

---
+++ src/Components/Blob/Blob.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import './Blob.css';
+export default class Blob extends Component {
+    render() {
+        return (
+            <div>Blob</div>
+        )
+    }
+}
